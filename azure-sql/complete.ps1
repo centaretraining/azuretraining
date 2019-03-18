d854002c5fa2d0b25e625b60578c0e74b5761921
Steve Hirscher <trashbin1@hotmail.com>
2019-03-18 15:01:10 -0500

    Create completed scripts for SQL and web app exercises

---
+++ azure-sql/complete.ps1
@@ -0,0 +1,51 @@
+$sqlAdminUserName = 'lunchadmin'
+$sqlAdminPassword = '%Lunch4U!'
+$sqlServerName = "$env:username-lunch-sql"
+$resourceGroupName = "$env:username-lunch-webapp-rg"
+$sqlDatabaseName = 'lunch-db'
+
+if (!(pwd).Path.EndsWith("azuretraining")) {
+    Write-Error "You must be in the azuretraining folder"
+    exit
+}
+if ($env.username -eq "") {
+    Write-Error 'The variable $env.username does not contain a user, aborting...'
+    exit
+}
+
+if (Get-Command "git.exe" -ErrorAction SilentlyContinue) 
+{ 
+    Write-Host "Getting latest version of course from github..."
+    git pull
+} else {
+    Write-Host "Warning - git not in path, skipping pull..."
+}
+
+Write-Host "Checking if resource group exists..."
+if ("true" -eq (az group show --name $resourceGroupName)) {
+    Write-Host "Resource group $resourceGroupName exists, deleting..."
+    az group delete --name $resourceGroupName
+}
+
+Write-Host "Creating resource group $resourceGroupName"
+az group create --name $resourceGroupName --location "North Central US"
+
+Write-Host "Creating SQL Server $sqlServerName"
+az sql server create `
+	--name $sqlServerName `
+	--resource-group $resourceGroupName `
+	--location northcentralus  `
+	--admin-user $sqlAdminUserName `
+	--admin-password $sqlAdminPassword
+
+Write-Host "Creating database $sqlDatabaseName"
+az sql db create `
+	--resource-group $resourceGroupName `
+	--server $sqlServerName `
+	--name $sqlDatabaseName `
+	--service-objective S0
+
+Write-Host "Creating firewall rule..."
+az sql server firewall-rule create -g $resourceGroupName -s $sqlServerName -n "allowAzure" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
+Write-Host "SQL Server created!"
---
+++ web-apps-powershell/complete.ps1
@@ -0,0 +1,78 @@
+$resourceGroupName = "$env:username-lunch-webapp-rg"
+$appServicePlanName = "$env:username-lunch-hp"
+$webAppServiceName = "$env:username-lunch-web-as"
+$apiAppServiceName = "$env:username-lunch-api-as"
+$sqlServerName = "$env:username-lunch-sql"
+$sqlDatabaseName = "lunch-db"
+$sqlAdminUserName = "lunchadmin"
+$sqlAdminPassword = "%Lunch4U!"
+
+Write-Host "Running SQL Server deployment script..."
+./azure-sql/complete.ps1
+
+Write-Host "Creating app service plan $appServicePlanName..."
+az appservice plan create -g $resourceGroupName `
+    --name $appServicePlanName `
+    --sku S1 `
+    --location northcentralus 
+
+Write-Host "Creating app service $webAppServiceName..."
+az webapp create -n $webAppServiceName `
+                -p $appServicePlanName `
+                -g $resourceGroupName `
+                --verbose
+
+az webapp show --name $webAppServiceName `
+                --resource-group $resourceGroupName `
+                --query defaultHostName `
+                -o table
+
+Write-Host "Creating app service $apiAppServiceName..."
+az webapp create -n $apiAppServiceName `
+                -p $appServicePlanName `
+                -g $resourceGroupName `
+                --verbose
+
+az webapp show --name $apiAppServiceName `
+                --resource-group $resourceGroupName `
+                --query defaultHostName `
+                -o table
+
+Write-Host "Building API project"
+dotnet publish ./web-apps-powershell/src/WebAppFoodOrder.Api/WebAppFoodOrder.Api.csproj -o ./publish/webapi
+Write-Host "Building Web project"
+dotnet publish ./web-apps-powershell/src/WebAppFoodOrder.Web/WebAppFoodOrder.Web.csproj -o ./publish/webapp
+    
+Write-Host "Compressing API..."
+Compress-Archive -Path ./web-apps-powershell/src/WebAppFoodOrder.Api/publish/webapi/* -DestinationPath ./web-apps-powershell/src/WebAppFoodOrder.Api/publish/webappapi.zip -Force
+Write-Host "Compressing web..."
+Compress-Archive -Path ./web-apps-powershell/src/WebAppFoodOrder.Web/publish/webapp/* -DestinationPath ./web-apps-powershell/src/WebAppFoodOrder.Web/publish/webappweb.zip -Force
+
+Write-Host "Deploying API..."
+az webapp deployment source config-zip  -g $resourceGroupName `
+                -n $apiAppServiceName `
+                --src ./web-apps-powershell/src/WebAppFoodOrder.Api/publish/webappapi.zip
+
+Write-Host "Deploying web..."
+az webapp deployment source config-zip  -g $resourceGroupName `
+                -n $webAppServiceName `
+                --src ./web-apps-powershell/src/WebAppFoodOrder.Web/publish/webappweb.zip
+
+Write-Host "Setting API app config..."
+$newAppSettings= "ApiDomain=https://$apiAppServiceName.azurewebsites.net"
+az webapp config appsettings set --settings $newAppSettings `
+                    -n $webAppServiceName `
+                    -g $resourceGroupName
+
+$connectionString = "Server=tcp:$sqlServerName.database.windows.net,1433;Initial Catalog=$sqlDatabaseName;Persist Security Info=False;User ID=$sqlAdminUserName;Password=$sqlAdminPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+az webapp config connection-string set -g $resourceGroupName `
+                    -n $apiAppServiceName -t SQLAzure `
+                    --settings MenuConnection=$connectionString OrderConnection=$connectionString
+
+Write-Host "Configuring CORS..."
+az webapp cors add -g $resourceGroupName -n $apiAppServiceName --allowed-origins '*'
+
+Write-Host "Done!"
+Write-Host "Website at https://$webAppServiceName.azurewebsites.net"
+
+& "start https://$webAppServiceName.azurewebsites.net" 
